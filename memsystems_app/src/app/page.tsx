d6ef75f8aaf83ac1b14bf0c10aad8de16565297b
johandev21 <johandev21@gmail.com>
2026-06-16 16:19:59 -0400

    initial migration setup

---
+++ memsystems_app/src/app/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div className="min-h-screen flex justify-center items-center">
+      <Button>Shadcn Button</Button>
+    </div>
+  );
+}
